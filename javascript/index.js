101cffebcde73fe2767d48eabef0ca567348b79b
Sarahwcode <sarahw181090@gmail.com>
2024-10-12 19:28:32 +0100

    adding date and time zone

--- javascript/index.js
+++ javascript/index.js
@@ -1,46 +1,46 @@
 function changeCity(event) {
-  if (event.target.value.length > 0) {
-    let currentTime = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM Do YYYY h:m a");
-    alert(`It is ${currentTime} in ${event.target.value}`);
-  }
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#city");
+  citiesElement.innerHTML = `
+    <div class="city">
+    <h2>
+        <div>${cityName}</div>
+        <div class="date">${cityTime.format("dddd, MMMM Do, YYYY")}</div>
+        <div class="time">${cityTime.format(
+          "H.mm.ss[<small>]A[</small>]"
+        )}</div>
+        </h2>
+      </div>
+  `;
 }
 
 let cityDate = document.querySelector("#cities");
 cityDate.addEventListener("change", changeCity);
 
-function timeAndDateZone() {
-  let londonDateElement = document.querySelector(".date");
-  londonDateElement.innerHTML = moment()
-    .tz("Europe/London")
-    .format("dddd, MMMM Do, YYYY");
-  let londonTimeElement = document.querySelector(".time");
-  londonTimeElement.innerHTML = moment().format("H.mm.ss[<small>]A[</small>]");
-  let parisDateElement = document.querySelector(".parisDate");
-  parisDateElement.innerHTML = moment()
-    .tz("Europe/Paris")
-    .format("dddd, MMMM Do, YYYY");
-  let parisTimeElement = document.querySelector(".parisTime");
-  parisTimeElement.innerHTML = moment()
-    .tz("Europe/Paris")
-    .format("H.mm.ss[<small>]A[</small>]");
-  let mexicoDateElement = document.querySelector(".mexicoDate");
-  mexicoDateElement.innerHTML = moment()
-    .tz("America/Mexico_City")
-    .format("dddd, MMMM Do, YYYY");
-  let mexicoTimeElement = document.querySelector(".mexicoTime");
-  mexicoTimeElement.innerHTML = moment()
-    .tz("America/Mexico_City")
-    .format("H.mm.ss[<small>]A[</small>]");
-  let faroDateElement = document.querySelector(".faroDate");
-  faroDateElement.innerHTML = moment()
-    .tz("Europe/Faro")
-    .format("dddd, MMMM Do, YYYY");
-  let faroTimeElement = document.querySelector(".faroTime");
-  faroTimeElement.innerHTML = moment()
-    .tz("Europe/Faro")
-    .format("H.mm.ss[<small>]A[</small>]");
-}
-timeAndDateZone();
-setInterval(timeAndDateZone, 1);
+//function timeAndDateZone() {
+
+// let londonDateElement = document.querySelector(".date");
+// londonDateElement.innerHTML = moment()
+// .tz("Europe/London")
+//.format("dddd, MMMM Do, YYYY");
+//let londonTimeElement = document.querySelector(".time");
+//londonTimeElement.innerHTML = moment().format("H.mm.ss[<small>]A[</small>]");
+//let parisDateElement = document.querySelector(".parisDate");
+//parisDateElement.innerHTML = moment()
+//.tz("Europe/Paris")
+//.format("dddd, MMMM Do, YYYY");
+//let parisTimeElement = document.querySelector(".parisTime");
+//parisTimeElement.innerHTML = moment()
+//.tz("Europe/Paris")
+//.format("H.mm.ss[<small>]A[</small>]");
+//let mexicoDateElement = document.querySelector(".mexicoDate");
+//mexicoDateElement.innerHTML = moment()
+//.tz("America/Mexico_City")
+//.format("dddd, MMMM Do, YYYY");
+//let mexicoTimeElement = document.querySelector(".mexicoTime");
+//mexicoTimeElement.innerHTML = moment()
+//.tz("America/Mexico_City")
+//.format("H.mm.ss[<small>]A[</small>]");
+//}
